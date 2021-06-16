5a0f5da565d6b4e21254b74f5c43f7bf6995c927
ian-hancock <46765905+ian-hancock@users.noreply.github.com>
2021-06-16 20:45:04 +1000

    Add files via upload

--- tfv1test/main.tf
+++ tfv1test/main.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = "~> 1"
 
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+    }
+  }
+
   backend "local" {
     path = "terraform.tfstate"
   }
@@ -14,6 +20,17 @@ data terraform_remote_state tf11 {
   }
 }
 
+provider "random" {}
+
+resource "random_integer" "integer" {
+  min = 1000
+  max = 9999
+}
+
 output readstatefile {
     value = data.terraform_remote_state.tf11.outputs.tfv11output
+}
+
+output "tfv1output" {
+  value = "tfv1-${random_integer.integer.result}"
 }
